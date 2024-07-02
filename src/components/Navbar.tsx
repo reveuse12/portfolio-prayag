19054d87ac4074815ff6843584008f2ceae90b06
reveuse12 <prayag129787@gmail.com>
2024-07-02 22:21:50 +0530

    style improvement

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,27 +23,27 @@ export function Navbar({ className }: { className?: string }) {
           <div className="  text-sm grid grid-cols-2 gap-10 p-4">
             <ProductItem
               title="Admin Dashboard"
-              href="/dashboard"
-              src=""
+              href="https://admindashboard-vert.vercel.app/"
+              src="/project1.png"
               description="Admin dashboard that focuses on employee management in company"
-            />
-            <ProductItem
-              title="AI-chat App"
-              href="https://ai-app-jet.vercel.app/"
-              src=""
-              description="unlocks the power of knowledge with information discovery and sharing."
             />
             <ProductItem
               title="AD exports"
               href="https://adexports.vercel.app/"
-              src=""
+              src="/project2.png"
               description="Discover our advanced CNC machining solutions including Multi-Tasking, 5-axis, milling, turning, CNC controls and automation."
             />
             <ProductItem
               title="Block-staking App"
               href="https://staking-app-pied.vercel.app/"
-              src=""
+              src="/project3.png"
               description="Our app is the only app that lets you buy crypto with a card or bank account and self-custody your assets—all in one place"
+            />
+            <ProductItem
+              title="AI-chat App"
+              href="https://ai-app-jet.vercel.app/"
+              src="/project4.png"
+              description="unlocks the power of knowledge with information discovery and sharing."
             />
           </div>
         </MenuItem>
--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -28,7 +28,7 @@ export const MenuItem = ({
     <div onMouseEnter={() => setActive(item)} className="relative ">
       <motion.p
         transition={{ duration: 0.3 }}
-        className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white"
+        className="cursor-pointer text-white hover:opacity-[0.9] dark:text-white"
       >
         {item}
       </motion.p>
@@ -70,7 +70,7 @@ export const Menu = ({
   return (
     <nav
       onMouseLeave={() => setActive(null)} // resets the state
-      className="relative rounded-full boder border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-4 px-8 py-6 "
+      className="relative rounded-full boder border-transparent dark:bg-black dark:border-white/[0.2] bg-black shadow-input flex justify-center space-x-4 px-8 py-6 "
     >
       {children}
     </nav>
@@ -92,13 +92,13 @@ export const ProductItem = ({
     <Link href={href} className="flex space-x-2">
       <Image
         src={src}
-        width={140}
-        height={70}
+        width={200}
+        height={100}
         alt={title}
-        className="flex-shrink-0 rounded-md shadow-2xl"
+        className="flex-shrink-0 bg-cover rounded-md shadow-2xl"
       />
       <div>
-        <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
+        <h4 className="text-xl font-bold mb-1 text-white dark:text-white">
           {title}
         </h4>
         <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
